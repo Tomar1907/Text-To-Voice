c58cf3b81fcb08cbcdb9226c1a0cff00d3454c2f
Tomar1907 <soumyatomar1908@gmail.com>
2025-04-18 10:56:56 +0530

    initial commit

--- script.js
+++ script.js
@@ -18,4 +18,5 @@ voiceSelect.addEventListener("change",()=>{
 document.querySelector("button").addEventListener("click", ()=>{
     speech.text = document.querySelector("textarea").value;
     window.speechSynthesis.speak(speech);
+
 })
